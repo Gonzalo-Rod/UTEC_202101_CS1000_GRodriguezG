4de965a646e1f2bc0cfd9905c633348309733776
Gonzalo Rodriguez <gonzalo.rodriguez.g@utec.edu.pe>
2021-05-30 18:00:57 -0500

    587 y 3054 a octal

--- Semana2/Ejercicios_S2.py
+++ Semana2/Ejercicios_S2.py
@@ -1,7 +1,9 @@
-base = 16
-num = 305
+#Ej 9 13
 
-#Primera Division
+base = 8
+num = int(input("Elige el numer que quieres convertir [587 / 3054]: "))
+
+#Primera ZeroDivisionE
 residuo = num % base
 cociente = num // base
 n1 = residuo
@@ -11,63 +13,14 @@ residuo = cociente % base
 cociente = cociente // base
 n2 = residuo
 
-#Tercera Division
+# Terceras division
 residuo = cociente % base
 cociente = cociente // base
 n3 = residuo
 
-if n1 == 10:
-  n1 = "a"
+#Cuarta Divisin
+residuo = cociente % base
+cociente = cociente // base
+n4 = residuo
 
-elif n1 == 11:
-  n1 = "b"
-
-elif n1 == 12:
-  n1 = "c"
-
-elif n1 == 13:
-  n1 = "d"
-
-elif n1 == 14:
-  n1 = "e"
-
-elif n1 == 15:
-  n1 = "f"
-
-if n2 == 10:
-  n2 = "a"
-
-elif n2 == 11:
-  n2 = "b"
-
-elif n2 == 12:
-  n2 = "c"
-
-elif n2 == 13:
-  n2 = "d"
-
-elif n2 == 14:
-  n2 = "e"
-
-elif n2 == 15:
-  n2 = "f"
-
-if n3 == 10:
-  n3 = "a"
-
-elif n3 == 11:
-  n3 = "b"
-
-elif n3 == 12:
-  n3 = "c"
-
-elif n3 == 13:
-  n3 = "d"
-
-elif n3 == 14:
-  n3 = "e"
-
-elif n3 == 15:
-  n3 = "f"
-
-print("{} = {}{}{}".format(num, n3, n2, n1))
+print("{} = {}{}{}{}".format(num, n4, n3, n2, n1))
