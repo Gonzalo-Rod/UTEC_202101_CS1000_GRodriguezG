96f22d38a50b4b7872ddb40a263e4c5f00fb91b1
Gonzalo Rodriguez <gonzalo.rodriguez.g@utec.edu.pe>
2021-05-30 17:54:36 -0500

    305 a hexadecimal

--- Semana2/Ejercicios_S2.py
+++ Semana2/Ejercicios_S2.py
@@ -1,46 +1,73 @@
-# Ej 7
-
-base = 2
-num = 254
+base = 16
+num = 305
 
 #Primera Division
 residuo = num % base
 cociente = num // base
-bit1 = residuo
+n1 = residuo
 
 #Segunda Division
 residuo = cociente % base
 cociente = cociente // base
-bit2 = residuo
+n2 = residuo
 
-# Terceras division
+#Tercera Division
 residuo = cociente % base
 cociente = cociente // base
-bit3 = residuo
+n3 = residuo
 
-#Cuarta Division
-residuo = cociente % base
-cociente = cociente // base
-bit4 = residuo
+if n1 == 10:
+  n1 = "a"
 
-#Quinta Division
-residuo = cociente % base
-cociente = cociente // base
-bit5 = residuo
+elif n1 == 11:
+  n1 = "b"
 
-#Sexta Division
-residuo = cociente % base
-cociente = cociente // base
-bit6 = residuo
+elif n1 == 12:
+  n1 = "c"
 
-#Septima Division
-residuo = cociente % base
-cociente = cociente // base
-bit7 = residuo
+elif n1 == 13:
+  n1 = "d"
 
-#Octava  Division
-residuo = cociente % base
-cociente = cociente // base
-bit8 = residuo
+elif n1 == 14:
+  n1 = "e"
 
-print("{} = {}{}{}{}{}{}{}{}".format(num,bit8, bit7, bit6, bit5, bit4, bit3, bit2, bit1))
+elif n1 == 15:
+  n1 = "f"
+
+if n2 == 10:
+  n2 = "a"
+
+elif n2 == 11:
+  n2 = "b"
+
+elif n2 == 12:
+  n2 = "c"
+
+elif n2 == 13:
+  n2 = "d"
+
+elif n2 == 14:
+  n2 = "e"
+
+elif n2 == 15:
+  n2 = "f"
+
+if n3 == 10:
+  n3 = "a"
+
+elif n3 == 11:
+  n3 = "b"
+
+elif n3 == 12:
+  n3 = "c"
+
+elif n3 == 13:
+  n3 = "d"
+
+elif n3 == 14:
+  n3 = "e"
+
+elif n3 == 15:
+  n3 = "f"
+
+print("{} = {}{}{}".format(num, n3, n2, n1))
