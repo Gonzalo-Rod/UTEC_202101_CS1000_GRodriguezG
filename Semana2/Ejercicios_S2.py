c8bd08b770a4f0b9053a6e1fc41a325a82ba8896
Gonzalo Rodriguez <gonzalo.rodriguez.g@utec.edu.pe>
2021-05-30 18:17:03 -0500

    15850 a hexadecimal

--- Semana2/Ejercicios_S2.py
+++ Semana2/Ejercicios_S2.py
@@ -1,71 +1,97 @@
-#Ej 14
+base = 16
+num = 15850
 
-base = 2
-num = 5785
-
-#Primera division
+#Primera Division
 residuo = num % base
 cociente = num // base
-bit1 = residuo
+n1 = residuo
 
-#Segunda division
+#Segunda Division
 residuo = cociente % base
 cociente = cociente // base
-bit2 = residuo
+n2 = residuo
 
-#Tercera division
+#Tercera Division
 residuo = cociente % base
 cociente = cociente // base
-bit3 = residuo
+n3 = residuo
 
-#Cuarta division
+#Cuarta Division
 residuo = cociente % base
 cociente = cociente // base
-bit4 = residuo
+n4 = residuo
 
-#Quinta division
-residuo = cociente % base
-cociente = cociente // base
-bit5 = residuo
 
-#Sexta division
-residuo = cociente % base
-cociente = cociente // base
-bit6 = residuo
+if n1 == 10:
+  n1 = "a"
 
-#Septima division
-residuo = cociente % base
-cociente = cociente // base
-bit7 = residuo
+elif n1 == 11:
+  n1 = "b"
 
-#Octava division
-residuo = cociente % base
-cociente = cociente // base
-bit8 = residuo
+elif n1 == 12:
+  n1 = "c"
 
-#Novena division
-residuo = cociente % base
-cociente = cociente // base
-bit9 = residuo
+elif n1 == 13:
+  n1 = "d"
 
-#Decima division
-residuo = cociente % base
-cociente = cociente // base
-bit10 = residuo
+elif n1 == 14:
+  n1 = "e"
 
-#Undecima division
-residuo = cociente % base
-cociente = cociente // base
-bit11 = residuo
+elif n1 == 15:
+  n1 = "f"
 
-#Duodecima division
-residuo = cociente % base
-cociente = cociente // base
-bit12 = residuo
+if n2 == 10:
+  n2 = "a"
 
-#Trigesima division
-residuo = cociente % base
-cociente = cociente // base
-bit13 = residuo
+elif n2 == 11:
+  n2 = "b"
 
-print("{} = {}{}{}{}{}{}{}{}{}{}{}{}{}".format(num, bit13, bit12, bit11, bit10, bit9, bit8, bit7, bit6, bit5, bit4, bit3, bit2, bit1))
+elif n2 == 12:
+  n2 = "c"
+
+elif n2 == 13:
+  n2 = "d"
+
+elif n2 == 14:
+  n2 = "e"
+
+elif n2 == 15:
+  n2 = "f"
+
+if n3 == 10:
+  n3 = "a"
+
+elif n3 == 11:
+  n3 = "b"
+
+elif n3 == 12:
+  n3 = "c"
+
+elif n3 == 13:
+  n3 = "d"
+
+elif n3 == 14:
+  n3 = "e"
+
+elif n3 == 15:
+  n3 = "f"
+
+if n4 == 10:
+  n4 = "a"
+
+elif n4 == 11:
+  n4 = "b"
+
+elif n4 == 12:
+  n4 = "c"
+
+elif n4 == 13:
+  n4 = "d"
+
+elif n4 == 14:
+  n4 = "e"
+
+elif n4 == 15:
+  n4 = "f"
+
+print("{} = {}{}{}{}".format(num,n4, n3, n2, n1))
