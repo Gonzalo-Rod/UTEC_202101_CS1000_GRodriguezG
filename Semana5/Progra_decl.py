384ec0a1b477211efe028f89c2504db072768c81
Gonzalo Rodriguez <gonzalo.rodriguez.g@utec.edu.pe>
2021-05-30 19:25:21 -0500

    Programacion Declarativa Ej4

--- Semana5/Progra_decl.py
+++ Semana5/Progra_decl.py
@@ -1,16 +1,14 @@
 
-print("Ejercicio 3")
+print("Ejercicio 4")
 print("-"*40)
-#lee 5 números enteros
-# desde teclado y almacénalos en una lista
-lista = []
-for i in range(5):
-    n = int(input())
-    lista.append(n)
-print(lista)
-print(lista)
+numeros = [1,2,3,4,5]
+pares = []
+for i in numeros:
+    if i%2 == 0:
+        pares.append(i)
+print(pares)
 
 #Programación declarativa
-lista = [int(input()) for i in range(5)]
-print(lista)
 
+pares = [i for i in numeros if (i % 2 == 0)]
+print(pares)
